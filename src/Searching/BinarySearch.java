2e9cff1e972683a8efd5a74e34cb526307b30148
amankumarthakur63 <amankumar005562@gmail.com>
2024-10-04 17:10:55 +0530

    commit this please

--- src/Searching/BinarySearch.java
+++ src/Searching/BinarySearch.java
@@ -1,2 +1,25 @@
-package Searching;public class BinarySearch {
+package Searching;
+
+public class BinarySearch {
+    public static int BS (int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 5, 7, 9, 10, 56, 83, 455};
+        int target = 83;
+        System.out.println(BS(nums, target));
+    }
 }
--- src/Searching/LinearSearch.java
+++ src/Searching/LinearSearch.java
@@ -1,2 +1,21 @@
-package Searching;public class LinearSearch {
+package Searching;
+
+public class LinearSearch {
+    public static int linear (int[] nums, int target) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int element = nums[i];
+            if (element == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {45,34,45,7,6,764,5,5,46,45,45,76,3};
+        int target = 3;
+        System.out.println(linear(nums, target));
+    }
 }
