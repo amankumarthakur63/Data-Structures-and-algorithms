697d6f863faf625e37bda10928a210ff5c008a78
amankumarthakur63 <amankumar005562@gmail.com>
2024-10-22 23:43:23 +0530

    commit this

--- src/LeetCode/MissingNumber.java
+++ src/LeetCode/MissingNumber.java
@@ -1,18 +1,18 @@
-package Arrays;
+package LeetCode;
 
 public class MissingNumber {
-    public static int missing (int[] arr) {
-        int n = arr.length + 1;
+
+    public static int missingNumber(int[] nums) {
+        int n = nums.length;
         int sum = n * (n + 1) / 2;
-        for (int num : arr) {
+        for (int num : nums) {
             sum = sum - num;
         }
         return sum;
     }
 
-
     public static void main(String[] args) {
         int[] arr = {3, 0, 1};
-        System.out.println(missing(arr));
+        System.out.println(missingNumber(arr));
     }
 }
