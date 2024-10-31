326b1699f1c76ece168a043458d139f91d401186
amankumarthakur63 <amankumar005562@gmail.com>
2024-10-31 23:20:16 +0530

    commit this please

---
+++ src/LeetCode/SameTree.java
@@ -0,0 +1,29 @@
+package LeetCode;
+
+public class SameTree {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            if (p == null || q == null) {
+                return p == q;
+            }
+            return p.val == q.val && isSameTree(p.left, q.left) && isSameTree (p.right, q.right);
+        }
+
+
+    public static void main(String[] args) {
+    }
+}
