5809340e4a29d26f0d6fbe52b13251fd51d96d87
amankumarthakur63 <amankumar005562@gmail.com>
2024-11-03 23:08:09 +0530

    commit this

---
+++ src/LeetCode/IntersectionOfTwoArray.java
@@ -0,0 +1,32 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class IntersectionOfTwoArray {
+
+    class Solution {
+        public int[] intersection(int[] nums1, int[] nums2) {
+            HashSet<Integer> set = new HashSet<>();
+            for(int num:nums1){
+                set.add(num);
+            }
+            ArrayList<Integer> ans = new ArrayList<>();
+            for(int num:nums2){
+                if(set.contains(num)){
+                    ans.add(num);
+                    set.remove(num);
+                }
+            }
+            int[] res=new int[ans.size()];
+            for(int i=0;i<ans.size();i++){
+                res[i]= ans.get(i);
+            }
+            return res;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
